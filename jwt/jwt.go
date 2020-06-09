0a774d10bdeb76ccaf53429e084aca080fd01bd8
karenahv <you@example.com>
2020-06-08 20:34:09 -0500

    verPerfil

---
+++ jwt/jwt.go
@@ -0,0 +1,30 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/Karenahv/twitter_en_go/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+/*GeneroJWT genera el encriptado con JWT */
+func GeneroJWT(t models.Usuario) (string, error) {
+	miClave := []byte("MastersdelDesarrollo_grupodeFacebook")
+	payload := jwt.MapClaims{
+		"email":            t.Email,
+		"nombre":           t.Nombre,
+		"apellidos":        t.Apellidos,
+		"fecha_nacimiento": t.FechaNacimiento,
+		"biografía":        t.Biografia,
+		"ubicacion":        t.Ubicacion,
+		"sititoweb":        t.SitioWeb,
+		"_id":              t.ID.Hex(),
+		"expe":             time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenStr, err := token.SignedString(miClave)
+	if err != nil {
+		return tokenStr, err
+	}
+	return tokenStr, nil
+}
---
+++ routers/login.go
@@ -0,0 +1,51 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/Karenahv/twitter_en_go/bd"
+	"github.com/Karenahv/twitter_en_go/jwt"
+	"github.com/Karenahv/twitter_en_go/models"
+)
+
+/* Login realiza el login del usuario si existe en la bd*/
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Usuario y/o Contraseña inválidos "+err.Error(), 400)
+		return
+	}
+	if len(t.Email) == 0 {
+		http.Error(w, "El email del usuario es requerido", 400)
+		return
+	}
+	documento, existe := bd.ItentoLogin(t.Email, t.Password)
+	if existe == false {
+		http.Error(w, "Usuario y/o Contraseña inválidos "+err.Error(), 400)
+		return
+	}
+	jwtkey, err := jwt.GeneroJWT(documento)
+	if err != nil {
+		http.Error(w, "Ocurrió un error al intentar generar el Token correspondiente "+err.Error(), 400)
+		return
+	}
+	resp := models.RespuestaLogin{
+		Token: jwtkey,
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resp)
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   jwtkey,
+		Expires: expirationTime,
+	})
+}
